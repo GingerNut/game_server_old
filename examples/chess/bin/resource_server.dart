3ab5a863c453714bd27fb31d8610d7b9de27c260
Stephen Poole <stephenmpoole@gmail.com>
2019-06-17 07:08:01 +0100

    chess started

---
+++ examples/chess/bin/resource_server.dart
@@ -0,0 +1,31 @@
+
+//  darC:\Users\Stephen\growing_games\game_server\examples\chess\bin\resource_server.dart
+
+import 'dart:io';
+
+import 'package:game_server/game_server.dart';
+import 'package:game_server/src/game_server/server_connection/http_connection.dart';
+
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+
+import '../lib/chess_injector.dart';
+
+
+void main() async{
+  String address = 'localhost';
+  int port = 8080;
+
+  GameServer gameServer = GameServer(ChessInjector());
+  await gameServer.db.testData();
+
+  var handler = webSocketHandler((webSocket) {
+
+    HttpConnection client = HttpConnection(webSocket);
+    gameServer.addConnection(client);
+
+  });
+
+  HttpServer server = await shelf_io.serve(handler, address, port);
+  print('Serving at ws://${server.address.host}:${server.port}');
+}
