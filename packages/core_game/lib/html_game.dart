c91a507bb5a4310918b87502b62957f4845b2107
Stephen Poole <stephenmpoole@gmail.com>
2019-07-07 18:18:28 +0100

    back to having code in one file for simplicity

--- packages/core_game/lib/html_game.dart
+++
@@ -1,4 +0,0 @@
-
-library core_game;
-
-export 'src/game/game.dart';
--- packages/core_game/lib/src/design/design.dart
+++
@@ -1,7 +0,0 @@
-library design;
-
-part 'color_palette.dart';
-part 'palette.dart';
-part 'theme.dart';
-part 'game_color.dart';
-part 'color_definitions.dart';
